e8d47cedf799df724ba040dd62bfea272aecc48d
Junior DAVON <juniordavon1998@gmail.com>
2022-03-01 09:26:51 +0100

    feat: add boilerplate code

---
+++ react-app/src/Calculator.js
@@ -0,0 +1,14 @@
+
+  
+  class Calculator extends React.Component {
+    render() {
+      return (
+        <div className="calculator">
+          <div className="calculator-panel">
+            <Panel />
+          </div>
+          
+        </div>
+      );
+    }
+  }
---
+++ react-app/src/Cells.js
@@ -0,0 +1,9 @@
+class Cell extends React.Component {
+    render() {
+      return (
+        <button className="cell">
+         
+        </button>
+      );
+    }
+  }
