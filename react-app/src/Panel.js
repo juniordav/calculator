a412074a19ac6b720062ebbc3fccbe92c0024f83
Junior DAVON <juniordavon1998@gmail.com>
2022-03-01 09:29:37 +0100

    feat: add boilerplate code

--- react-app/src/Panel.js
+++ react-app/src/Panel.js
@@ -4,22 +4,20 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
   
       return (
         <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
+          <div className="panel-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
           </div>
-          <div className="board-row">
+          <div className="panel-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
           </div>
-          <div className="board-row">
+          <div className="panel-row">
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
